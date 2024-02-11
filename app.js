20de9b823fc715e0d36418c439aa3213f212ed86
scara02 <221284@astanait.edu.kz>
2024-02-11 21:45:22 +0600

    commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('register');
